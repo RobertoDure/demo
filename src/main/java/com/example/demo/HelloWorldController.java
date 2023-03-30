422f13ea31a6c5b580da899d9ae5645550b40d61
Roberto <robertodure1990@gmail.com>
2023-03-30 18:04:18 +0100

    update

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,13 +1,11 @@
-package com.springboot.first.app;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
-	// GET HTTP Method
-	// http://localhost:8080/hello-world
+
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World!";
